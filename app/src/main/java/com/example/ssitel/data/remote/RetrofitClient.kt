d07642ca6b66083e65790d9d6a14e1088a1e1a85
Leonardo Rodriguez <165513433+Bad-Goty@users.noreply.github.com>
2025-11-27 16:41:46 -0600

    Enhance RetrofitClient with explanatory comments
    Added comments to clarify the purpose of the interceptor and client setup.

--- app/src/main/java/com/example/ssitel/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/ssitel/data/remote/RetrofitClient.kt
@@ -8,19 +8,19 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     private const val BASE_URL = "http://192.168.100.9:3000/"
 
-    private val interceptor = HttpLoggingInterceptor().apply {
+    private val interceptor = HttpLoggingInterceptor().apply {  // <- Esto imprime todas las peticiones y respuestas HTTP (URL, Headers, JSON enviado, JSON recibido)
         level = HttpLoggingInterceptor.Level.BODY
     }
 
-    private val client = OkHttpClient.Builder()
+    private val client = OkHttpClient.Builder()  // <- IMPORTANTE para agregar interceptores, timeouts y hasta autentificacion de TOKENS, si no se agrega RETROFIT usa uno por defecto sin mostrar lo mencionado
         .addInterceptor(interceptor)
         .build()
 
     val instance: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        Retrofit.Builder()                                       // Crea el objeto Retrofit
+            .baseUrl(BASE_URL)                                   // Establece la UTL base
+            .client(client)                                      //se conecta el cliente
+            .addConverterFactory(GsonConverterFactory.create())  //Retrofit necesita convertir JSON <-> objetos Kotlin.
+            .build()                                             // Construye y devuelve la instancia
     }
 }
